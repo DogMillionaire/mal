3442909f890b75d049fa587628236fa81aa25ebb
DogMillionaire <48490520+DogMillionaire@users.noreply.github.com>
2024-10-02 13:01:03 -0700

    Step 3 (#21)
    Co-authored-by: Peter Richards <p_j_richards@hotmail.com>

--- impls/python3.2/mal_error.py
+++ impls/python3.2/mal_error.py
@@ -7,6 +7,13 @@ class MalEOFError(MalError):
 class MalNoInputError(MalError):
     pass
 
+class MalSymbolNotFoundError(MalError):
+    symbol: str
+
+    def __init__(self, symbol: str):
+        super().__init__(f"Symbol {symbol} not found in the environment")
+        self.symbol = symbol
+
 class MalSyntaxError(MalError):
     start: int
 
